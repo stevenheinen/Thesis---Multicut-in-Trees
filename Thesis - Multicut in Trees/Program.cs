e9245e769fe73e73670d59395a7f278931941515
Heinen,S.A. (Steven) <s.a.heinen@students.uu.nl>
2020-12-08 17:03:53 +0100

    Initial commit: add empty project

---
+++ Thesis - Multicut in Trees/Program.cs
@@ -0,0 +1,14 @@
+﻿// This program was written by Steven Heinen (mailto:s.a.heinen@uu.nl) within a final thesis project of the Computing Science master program at Utrecht University under supervision of J.M.M. van Rooij (mailto:j.m.m.vanrooij@uu.nl).
+
+using System;
+
+namespace MulticutInTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
