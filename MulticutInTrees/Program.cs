7edb6dc2f05fe616f108cfc45a4ef15591f8087d
Heinen,S.A. (Steven) <s.a.heinen@students.uu.nl>
2020-12-09 17:14:44 +0100

    Rename files and folder containing spaces

---
+++ MulticutInTrees/Program.cs
@@ -0,0 +1,14 @@
+// This code was written between November 2020 and October 2021 by Steven Heinen (mailto:s.a.heinen@uu.nl) within a final thesis project of the Computing Science master program at Utrecht University under supervision of J.M.M. van Rooij (mailto:j.m.m.vanrooij@uu.nl).
+
+using System;
+
+namespace MulticutInTrees
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
