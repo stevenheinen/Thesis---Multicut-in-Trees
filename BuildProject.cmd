786c11b8fd14d48ae9283aabf94eb246cbd1c979
Heinen,S.A. (Steven) <s.a.heinen@students.uu.nl>
2021-04-13 11:37:28 +0200

    Update BuildProject.cmd to use longer timeouts and write output

--- BuildProject.cmd
+++ BuildProject.cmd
@@ -1,9 +1,9 @@
 echo Building...
 dotnet build "MulticutInTrees" -c=Debug
-ping -n 5 127.0.0.1 >NUL
+ping -n 10 127.0.0.1
 dotnet build "MulticutInTrees" -c=VerboseDebug
-ping -n 5 127.0.0.1 >NUL
+ping -n 10 127.0.0.1
 dotnet build "MulticutInTrees" -c=Release
-ping -n 5 127.0.0.1 >NUL
+ping -n 10 127.0.0.1
 dotnet build "MulticutInTrees" -c=Experiment
-ping -n 5 127.0.0.1 >NUL
+ping -n 10 127.0.0.1
